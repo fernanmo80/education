c1ea0bc0e3a35511acdb078dc839a1fe47fb36f7
fernanmo80 <fernanmo80@gmail.com>
2013-06-20 16:10:16 -0400

    comment test

--- M101P/pymongo_getting_started.py
+++ M101P/pymongo_getting_started.py
@@ -11,6 +11,10 @@ db = connection.test
 # handle to names collection
 names = db.names
 
+# save in collection names
+#db.names.save({'name':'Alonso'})
+
+
 item = names.find_one()
 
 print item['name']
